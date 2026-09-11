4d85609b16e79d7bf086b4e9d6e02e2187fb8b0b
sashwatmohanty <sashwatmohanty18@gmail.com>
2026-09-11 20:50:46 +0530

    dggd

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-const __dirname = import.meta.dirname;
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
